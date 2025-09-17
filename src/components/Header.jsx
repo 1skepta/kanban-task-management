9c3b76853ee961ea55a5025fcdaf2b906a35c5e9
1skepta <prospaul999@gmail.com>
2025-09-17 16:18:47 +0000

    working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,17 @@ import { ChevronDown, Plus, MoreVertical } from "lucide-react";
 
 function Header() {
   return (
-    <div className="bg-white flex justify-between px-6 py-6">
+    <div className="bg-white flex justify-between px-6 py-6 md:px-9 md:py-8">
       <div className="flex items-center">
         <div className="flex">
           <img src="logo.png" alt="company logo" className="w-9" />
-          <h1 className="font-bold ml-3 text-xl">kanban</h1>
+          <h1 className="font-bold ml-3 mr-9 text-2xl hidden md:block">
+            kanban
+          </h1>
         </div>
         <div className="flex items-center">
           <h1 className="font-semibold text-xl">Platform Launch</h1>
-          <ChevronDown size={20} className="text-[#635fc7]" />
+          <ChevronDown size={20} className="text-[#635fc7] md:hidden" />
         </div>
       </div>
       <div className="flex items-center">
