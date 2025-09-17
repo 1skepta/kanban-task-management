80382cd6eaff3ff8a3fb35289bd9af8ff04e20a9
1skepta <prospaul999@gmail.com>
2025-09-17 16:28:09 +0000

    working

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Eye, EyeOff } from "lucide-react";
+import { Eye, EyeOff, KeyboardMusic } from "lucide-react";
 
 function Sidebar() {
   return (
@@ -7,8 +7,26 @@ function Sidebar() {
         <Eye />
       </div> */}
 
-      <div className="bg-white ">
-        <div className="flex items-center"></div>
+      <div className="bg-white hidden md:flex flex-col px-6">
+        <span>ALL BOARDS (3)</span>
+        <div className="flex items-center">
+          <ul className="flex flex-col">
+            <li className="flex flex-row">
+              <div className="flex items-center">
+                <KeyboardMusic /> <span>Platform Launch</span>
+              </div>
+            </li>
+            <li>
+              <KeyboardMusic /> <span>Marketing Plan</span>
+            </li>
+            <li>
+              <KeyboardMusic /> <span>Roadmap</span>
+            </li>
+            <li>
+              <KeyboardMusic /> <span>Create New Board</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
