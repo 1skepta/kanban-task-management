ce6a83a57fa874e01e4c515aafbd39e06752d040
1skepta <prospaul999@gmail.com>
2025-09-17 15:18:24 +0000

    working

--- src/components/EmptyTask.jsx
+++ src/components/EmptyTask.jsx
@@ -1,11 +1,17 @@
 import { Plus } from "lucide-react";
+
 function EmptyTask() {
   return (
-    <div>
-      <span>The board is empty. Create a new column to get started.</span>
-      <span className="text-white bg-[#635fc7]">
-        <Plus /> Add new Column
-      </span>
+    <div className="h-screen w-full flex items-center justify-center">
+      <div className="px-8 text-center">
+        <span className="font-semibold text-gray-400 block mb-4">
+          The board is empty. Create a new column to get started.
+        </span>
+        <div className="text-white bg-[#635fc7] px-4 py-2 font-semibold flex items-center gap-2 justify-center rounded-3xl cursor-pointer hover:bg-[#4e4bb8] transition">
+          <Plus size={18} />
+          <span>Add new Column</span>
+        </div>
+      </div>
     </div>
   );
 }
